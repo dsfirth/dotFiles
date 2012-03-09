c5c91dfe22713be798907d9997662586f2e5f34d
Darren S. Firth <dsfirth@gmail.com>
2012-03-09 18:21:35 -0500

    un-comment mklink calls

--- install-win32.cmd
+++ install-win32.cmd
@@ -1,8 +1,8 @@
 @if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
 
-@rem call mklink %HOME%\_gvimrc .\gvimrc
-@rem call mklink %HOME%\_vimrc .\vimrc
+call mklink %HOME%\_gvimrc .\gvimrc
+call mklink %HOME%\_vimrc .\vimrc
 
 call mkdir %HOME%\.vim\bundle
 call git clone https://github.com/gmarik/vundle.git %HOME%/.vim/bundle/vundle
