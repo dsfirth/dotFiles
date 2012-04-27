05bb90b294b4eaace8c40c597b7b36c6c34eb054
Darren S. Firth <dfirth@toolboxsolutions.com>
2012-04-27 16:39:05 -0400

    change to Hard links (symbolic links require Admin privileges)

--- install-win32.cmd
+++ install-win32.cmd
@@ -1,8 +1,8 @@
 @if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
 
-call mklink %HOME%\_gvimrc .\gvimrc
-call mklink %HOME%\_vimrc .\vimrc
+call mklink /h %HOME%\_gvimrc .\gvimrc
+call mklink /h %HOME%\_vimrc .\vimrc
 
 call mkdir %HOME%\.vim\bundle
 call git clone https://github.com/gmarik/vundle.git %HOME%/.vim/bundle/vundle
