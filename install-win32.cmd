5285e1fbc3f6f59515997dc6136ca29050333ec1
Darren S. Firth <dsfirth@gmail.com>
2015-01-11 13:24:58 -0800

    Switch to lightline.vim and clean up

--- install-win32.cmd
+++ install-win32.cmd
@@ -1,10 +1,12 @@
-if not exist "%HOME%" set HOME=%HOMEDRIVE%%HOMEPATH%
-if not exist "%HOME%" set HOME=%USERPROFILE%
+@if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
+@if not exist "%HOME%" @set HOME=%USERPROFILE%
 
+@rem Installing Curl on Windows
+@mklink /H %ProgramFiles(x86)%\Git\cmd\curl.cmd %CD%\lib\curl.cmd
 call mklink /h %HOME%\_vimrc %CD%\vimrc
 
-call mkdir %HOME%\.vim\bundle
-call git clone https://github.com/gmarik/Vundle.vim.git %HOME%\.vim\bundle\Vundle.vim
+call mkdir %HOME%\vimfiles\bundle
+call git clone https://github.com/gmarik/Vundle.vim.git %HOME%\vimfiles\bundle\Vundle.vim
 
 gvim -c PluginInstall! -c PluginClean -c qa
 
