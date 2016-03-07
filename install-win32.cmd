b1f3f01dbdd92bb6a5517bb4dd1044524bf388b1
Darren Firth <dfirth@microsoft.com>
2016-03-06 22:00:19 -0800

    fix paths in Win32 installer

--- install-win32.cmd
+++ install-win32.cmd
@@ -2,18 +2,18 @@
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
 
 REM Installing Curl on Windows
-call mklink /H "%ProgramFiles(x86)%\Git\cmd\curl.cmd" "%CD%\lib\curl.cmd"
+call mklink /H "%ProgramFiles%\Git\cmd\curl.cmd" "%CD%\lib\curl.cmd"
 
 call mklink /H "%HOME%\_vimrc" "%CD%\vimrc"
 
-IF NOT EXIST "%HOME%/vimfiles/bundle/vundle" (
-	call mkdir "%HOME%\vimfiles\bundle"
+IF NOT EXIST "%HOME%/.vim/bundle/vundle" (
+	call mkdir "%HOME%\.vim\bundle"
 )
 
-IF NOT EXIST "%HOME%/vimfiles/bundle/Vunde.vim" (
-	call git clone https://github.com/gmarik/Vundle.vim.git "%HOME%/vimfiles/bundle/Vundle.vim"
+IF NOT EXIST "%HOME%/.vim/bundle/Vunde.vim" (
+	call git clone https://github.com/gmarik/Vundle.vim.git "%HOME%/.vim/bundle/Vundle.vim"
 ) ELSE (
-	call cd "%HOME%/vimfiles/bundle/Vunde.vim"
+	call cd "%HOME%/.vim/bundle/Vunde.vim"
 	call git pull
 	call cd %HOME%
 )
