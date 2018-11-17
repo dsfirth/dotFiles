55919bf075b2e669e1a238c0ae2367942d7773de
Darren Firth <dfirth@microsoft.com>
2018-11-16 22:13:00 -0800

    Simplify $env:HOME usage to just $HOME

--- make.ps1
+++ make.ps1
@@ -8,16 +8,16 @@ switch ($Target)
 {
     'all'   { $_ = "update" }
     'install' {
-        Write-Output "Backing up existing .vim and _vimrc to $env:HOME/.vim.bak, $env:HOME/_vimrc.bak,"
-        if (Test-Path -Path "$env:HOME\.vim") { Move-Item -Path "$env:HOME\.vim" -Destination "$env:HOME\.vim.bak" }
-        if (Test-Path -Path "$env:HOME\_vimrc") { Move-Item -Path "$env:HOME\_vimrc" -Destination "$env:HOME\_vimrc.bak" }
+        Write-Output "Backing up existing .vim and _vimrc to $HOME/.vim.bak, $HOME/_vimrc.bak,"
+        if (Test-Path -Path "$HOME\.vim") { Move-Item -Path "$HOME\.vim" -Destination "$HOME\.vim.bak" }
+        if (Test-Path -Path "$HOME\_vimrc") { Move-Item -Path "$HOME\_vimrc" -Destination "$HOME\_vimrc.bak" }
 
         Write-Output "initializing submodules,"
         git submodule update --init
 
         Write-Output "linking up new .vim and _vimrc,"
-        New-Item -Path "$env:HOME\.vim" -ItemType Junction -Value "$PSScriptRoot\vim"
-        New-Item -Path "$env:HOME\_vimrc" -ItemType HardLink -Value "$PSScriptRoot\vimrc"
+        New-Item -Path "$HOME\.vim" -ItemType Junction -Value "$PSScriptRoot\vim"
+        New-Item -Path "$HOME\_vimrc" -ItemType HardLink -Value "$PSScriptRoot\vimrc"
         Write-Output "All done.  Happy Vimming!"
     }
     'update' {
@@ -28,6 +28,6 @@ switch ($Target)
         Invoke-WebRequest -Uri "https://tpo.pe/pathogen.vim" -OutFile "$PSScriptRoot\vim\autoload\pathogen.vim"
      }
     'clean' {
-        Remove-Item -Path "$env:HOME\.vim","$env:HOME\_vimrc" -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path "$HOME\.vim","$HOME\_vimrc" -Recurse -Force -ErrorAction Ignore
     }
 }
